49c273d744ddb58d15c6d7deab1b1bca5df37c0a
Heidy Arroyave <arroyaveheidy7@gmail.com>
2023-04-11 18:48:15 -0500

    Revert "Revert "feat: menu, filtro por tipo y fechas y alerta generico""
    This reverts commit e993af2e114852edbcd348e27cacd55242304b77.

--- lib/src/domain/repositories/reporte_repositories.dart
+++ lib/src/domain/repositories/reporte_repositories.dart
@@ -1,5 +1,7 @@
+import '../requests/reporte_filtro_request.dart';
 import '../requests/reporte_request.dart';
 
 abstract class ReporteRepositories {
   Future<List<ReporteRequest>> reporteGeneral();
+  Future<List<ReporteRequest>> reporteFiltrado(ReporteFiltroRequest filtro);
 }
