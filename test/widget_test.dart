ea2c7f95ea9da072aa9bc07f6710c39230ef7f86
Heidy Arroyave <arroyaveheidy7@gmail.com>
2023-04-11 18:30:16 -0500

    Revert "mm"
    This reverts commit 7f3bf7886dbfd58dd973a4d511a6e9245cd695db.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@ import 'package:alegra_store/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MaterialApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
